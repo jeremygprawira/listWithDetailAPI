07e1ea0498e53cf5243136d3edaf608655a6df5b
Jeremy <jeremygprawira@gmail.com>
2022-08-06 10:24:21 +0700

    Initial Commit

---
+++ listWithDetailAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listWithDetailAPI
+//
+//  Created by Jeremy Gerald Prawira on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
